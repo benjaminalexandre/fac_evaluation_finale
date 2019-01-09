b1f1c323d97ccf7515ed28abc516b2ffc6424a71
Benjamin Alexandre <benjamin.alexandre71@orange.fr>
2019-01-09 17:05:03 +0100

    Dernier commit

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Energies collectives</title>
+    <link rel="stylesheet" href="css/general.css">
+    <div style="width: 0; height: 0;">
+        <?php include('img/sprite_symbol_pictos.svg');?>
+    </div>
+</head>
+<body>
+<header>
+    <div class="slide">
+        <nav class="menu">
+            <div class="logo-site">
+                <img src="logos/EnergiesCollectives_rectangle_Logo_ajuste.svg" />
+            </div>
+            <div class="container" onclick="myFunction(this)">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
+        </nav>
+        <div class="navbar">
+            <ul>
+                <li class="link">
+                    <a href="#">En savoir plus</a>
+                    <svg class="Icon fleche-droite-menu" aria-hidden="true">
+                        <use xlink:href="#fleche_droite"></use>
+                    </svg>
+                </li>
+                <li class="link">
+                    <a href="#">Nous découvrir</a>
+                    <svg class="Icon fleche-droite-menu" aria-hidden="true">
+                        <use xlink:href="#fleche_droite"></use>
+                    </svg>
+                </li>
+                <li class="link">
+                    <a href="#">Nos installations</a>
+                    <svg class="Icon fleche-droite-menu" aria-hidden="true">
+                        <use xlink:href="#fleche_droite"></use>
+                    </svg>
+                </li>
+                <li class="link">
+                    <a href="#">Economison</a>
+                    <svg class="Icon fleche-droite-menu" aria-hidden="true">
+                        <use xlink:href="#fleche_droite"></use>
+                    </svg>
+                </li>
+                <li class="link">
+                    <a href="#">En savoir plus</a>
+                    <svg class="Icon fleche-droite-menu" aria-hidden="true">
+                        <use xlink:href="#fleche_droite"></use>
+                    </svg>
+                </li>
+                <li class="button">
+                    <button>Souscrire</button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="content">
+        <div class="logo">
+            <img src="./logos/EnergiesCollectivesLogo_ss_texte.svg"/>
+        </div>
+        <div class="titreTxt">
+            <h1>L'énergie est notre affaire</h1>
+            <h2>Investissons dans la transitions énergétique</h2>
+        </div>
+        <div class="logo-content">
+            <div class="coop">
+                <div class="logo-content-img">
+                    <svg class="Icon picto-logo" aria-hidden="true">
+                        <use xlink:href="#picto_coop"></use>
+                    </svg>
+                </div>
+                <p>COOPÉRATEURS<br/>
+                190</p>
+            </div>
+            <div class="photovoltaique">
+                <div class="logo-content-img">
+                    <svg class="Icon picto-logo" aria-hidden="true">
+                        <use xlink:href="#picto_photovoltaique"></use>
+                    </svg>
+                </div>
+                <p>INSTALLATIONS<br/>
+                8</p>
+            </div>
+            <div class="energie">
+                <div class="logo-content-img">
+                    <svg class="Icon picto-logo" aria-hidden="true">
+                        <use xlink:href="#picto_production_energie"></use>
+                    </svg>
+                </div>
+                <p>PRODUCTION ANNUELLE<br/>
+                    1441 MWh</p>
+            </div>
+            <div class="capital">
+                <div class="logo-content-img">
+                    <svg class="Icon picto-logo" aria-hidden="true">
+                        <use xlink:href="#picto_capital"></use>
+                    </svg>
+                </div>
+                <p>CAPITAL<br/>
+                112950€</p>
+            </div>
+        </div>
+    </div>
+    <div class="bg-point">
+        <div class="paragraphe">
+            <div class="title">
+                Restabat ut Caesar post haec properaret accitus et abstergendae causa suspicionis sororem suam, eius uxorem.
+            </div>
+            <div class="text">
+                Constantius ad se tandem desideratam venire multis fictisque blanditiis hortabatur. quae licet ambigeret metuens saepe cruentum, spe tamen quod eum lenire poterit ut germanum profecta, cum Bithyniam introisset, in statione quae Caenos Gallicanos appellatur, absumpta est vi febrium repentina. cuius post obitum maritus contemplans cecidisse fiduciam qua se fultum existimabat, anxia cogitatione, quid moliretur haerebat.
+            </div>
+        </div>
+    </div>
+
+    <div class="btn-plus">
+        <button>
+            Comprendre notre modèle de production
+            <svg class="Icon plus-logo" aria-hidden="true">
+                <use xlink:href="#plus"></use>
+            </svg>
+        </button>
+    </div>
+
+    <div class="bg-point">
+        <div class="paragraphe">
+            <div class="title">
+                Quibus occurrere bene pertinax miles explicatis ordinibus parans hastisque feriens scuta qui habitus iram pugnantium concitat.
+            </div>
+            <div class="text">
+                Et dolorem proximos iam gestu terrebat sed eum in certamen alacriter consurgentem revocavere ductores rati intempestivum anceps subire certamen cum haut longe muri distarent, quorum tutela securitas poterat in solido locari cunctorum.
+            </div>
+        </div>
+    </div>
+
+    <div class="btn-souscrire">
+        <button>
+            Souscrire
+            <svg class="Icon plus-logo" aria-hidden="true">
+                <use xlink:href="#schema_souscrire"></use>
+            </svg>
+        </button>
+    </div>
+
+    <div class="actu">
+        <div class="box">
+            <div class="head-box">
+                <span class="titre-box">ACTUALITÉS</span>
+                <svg class="Icon fleche-droite-actu" aria-hidden="true">
+                    <use xlink:href="#fleche_droite"></use>
+                </svg>
+            </div>
+            <div class="img-box">
+                <img src="Photos/IMG_20170121_122143.jpg"/>
+                <div class="box-text actu-text">
+                    Atque, ut Tullius ait, ut etiam ferae fame monitae plerumque ad eum locum ubi aliquando pastae sunt revertuntur, ita homines instar turbinis degressi montibus impeditis et arduis loca petivere mari confinia, per quae viis latebrosis sese convallibusque occultantes cum appeterent noctes luna etiam tum cornuta ideoque nondum solido splendore fulgente nauticos observabant quos cum in somnum sentirent effusos per ancoralia, quadrupedo gradu repentes seseque suspensis passibus iniectantes in scaphas eisdem sensim nihil opinantibus adsistebant et incendente aviditate saevitiam ne cedentium quidem ulli parcendo obtruncatis omnibus merces opimas velut viles nullis repugnantibus avertebant. haecque non diu sunt perpetrata.
+                </div>
+            </div>
+            <svg class="Icon plus-logo-box plus-logo-actu" aria-hidden="true">
+                <use xlink:href="#plus"></use>
+            </svg>
+        </div>
+    </div>
+
+    <div class="demarche">
+        <div class="box">
+            <div class="head-box">
+                <span class="titre-box">NOTRE DÉMARCHE</span>
+            </div>
+            <div class="img-box">
+                <img src="Photos/IMG_4106.jpg"/>
+                <div class="box-text">
+                    Atque, ut Tullius ait, ut etiam ferae fame monitae plerumque ad eum locum ubi aliquando pastae sunt revertuntur, ita homines instar turbinis degressi montibus impeditis et arduis loca petivere mari confinia, per quae viis latebrosis sese convallibusque occultantes cum appeterent noctes luna etiam tum cornuta ideoque nondum solido splendore fulgente nauticos observabant quos cum in somnum sentirent effusos per ancoralia, quadrupedo gradu repentes seseque suspensis passibus iniectantes in scaphas eisdem sensim nihil opinantibus adsistebant et incendente aviditate saevitiam ne cedentium quidem ulli parcendo obtruncatis omnibus merces opimas velut viles nullis repugnantibus avertebant. haecque non diu sunt perpetrata.
+                </div>
+            </div>
+            <svg class="Icon plus-logo-box" aria-hidden="true">
+                <use xlink:href="#plus"></use>
+            </svg>
+        </div>
+    </div>
+
+    <div class="installation">
+        <div class="box">
+            <div class="head-box">
+                <span class="titre-box">NOS INSTALLATIONS</span>
+            </div>
+            <div class="img-box">
+                <img src="Photos/IMG_4106.jpg"/>
+                <div class="box-text">
+                    Atque, ut Tullius ait, ut etiam ferae fame monitae plerumque ad eum locum ubi aliquando pastae sunt revertuntur, ita homines instar turbinis degressi montibus impeditis et arduis loca petivere mari confinia, per quae viis latebrosis sese convallibusque occultantes cum appeterent noctes luna etiam tum cornuta ideoque nondum solido splendore fulgente nauticos observabant quos cum in somnum sentirent effusos per ancoralia, quadrupedo gradu repentes seseque suspensis passibus iniectantes in scaphas eisdem sensim nihil opinantibus adsistebant et incendente aviditate saevitiam ne cedentium quidem ulli parcendo obtruncatis omnibus merces opimas velut viles nullis repugnantibus avertebant. haecque non diu sunt perpetrata.
+                </div>
+            </div>
+            <div class="btn">
+                <button>Voir toutes nos installations</button>
+            </div>
+            <svg class="Icon plus-logo-box" aria-hidden="true">
+                <use xlink:href="#plus"></use>
+            </svg>
+        </div>
+    </div>
+
+    <div class="temoignages">
+        <div class="box">
+            <div class="head-box">
+                <span class="titre-box">TÉMOIGNAGES</span>
+                <svg class="Icon fleche-droite-temoignages" aria-hidden="true">
+                    <use xlink:href="#fleche_droite"></use>
+                </svg>
+            </div>
+            <div class="img-box">
+                <img src="Photos/IMG_4106.jpg"/>
+                <div class="box-text">
+                    Atque, ut Tullius ait, ut etiam ferae fame monitae plerumque ad eum locum ubi aliquando pastae sunt revertuntur, ita homines instar turbinis degressi montibus impeditis et arduis loca petivere mari confinia, per quae viis latebrosis sese convallibusque occultantes cum appeterent noctes luna etiam tum cornuta ideoque nondum solido splendore fulgente nauticos observabant quos cum in somnum sentirent effusos per ancoralia, quadrupedo gradu repentes seseque suspensis passibus iniectantes in scaphas eisdem sensim nihil opinantibus adsistebant et incendente aviditate saevitiam ne cedentium quidem ulli parcendo obtruncatis omnibus merces opimas velut viles nullis repugnantibus avertebant. haecque non diu sunt perpetrata.
+                </div>
+            </div>
+            <svg class="Icon plus-logo-box" aria-hidden="true">
+                <use xlink:href="#plus"></use>
+            </svg>
+        </div>
+    </div>
+</main>
+<footer>
+    <div class="bg-footer">
+        <div class="footer">
+            <div class="contact">
+                <div class="fb">Nous suivre
+                    <svg class="Icon network" aria-hidden="true">
+                        <use xlink:href="#facebook"></use>
+                    </svg>
+                </div>
+                <div class="mail">Nous contacter
+                    <svg class="Icon network" aria-hidden="true">
+                        <use xlink:href="#mail"></use>
+                    </svg></div>
+                <div class="recherche">Rechercher
+                    <svg class="Icon network" aria-hidden="true">
+                        <use xlink:href="#search"></use>
+                    </svg></div>
+            </div>
+            <div class="newsletter">
+                <label>Abonnez-vous à la Newsletter</label><br/>
+                <input type="text"/>
+                <input type="button" value="ok"/>
+            </div>
+            <div class="partenaires-box">
+                <span>Nos partenaires</span>
+                <div class="partenaires-img"><img src="partenaires/energie-partagee_logo.png"></div>
+                <div class="partenaires-img"><img src="partenaires/arton110.jpg"></div>
+                <div class="partenaires-img"><img src="partenaires/paca_logo.png"></div>
+                <div class="partenaires-img"><img src="partenaires/arton124.jpg"></div>
+                <div class="partenaires-img"><img src="partenaires/Initiative-nord-hautes-alpes_logo.png"></div>
+                <div class="partenaires-img"><img src="partenaires/enercoop_logo.png"></div>
+                <div class="partenaires-img"><img src="partenaires/parc_ecrins.png"></div>
+                <div class="partenaires-img"><img src="partenaires/udess-05_logo-seul.png"></div>
+                <div class="partenaires-img"><img src="partenaires/omnis_logo.png"></div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+
+<script>
+    function myFunction(x) {
+        x.classList.toggle("change");
+        if(document.getElementsByClassName("navbar")[0].style.display === "block")
+            document.getElementsByClassName("navbar")[0].style.display = "none";
+        else
+            document.getElementsByClassName("navbar")[0].style.display = "block";
+    }
+</script>
+
+</html>
